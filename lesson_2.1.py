c36721810ce83549d08b04348612d2f8f9e7b30b
Alena-Vlad <lazarevaa.s@yandex.ru>
2020-04-29 15:49:34 +0300

    Урок 2 Задание 1

---
+++ lesson_2.1.py
@@ -0,0 +1,8 @@
+result_list = ['Алена', 175, 153, False, 'Maksim']
+
+print(result_list, "is of type", type(result_list))
+print(type([]) is list)
+print(type([]) is not list)
+print(type(()) is tuple)
+print(type({}) is dict)
+print(type({}) is not list)
