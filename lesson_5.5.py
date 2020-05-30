d75acef3fda1fc66d9c77c19ff6f425d9c60eaf9
Alena-Vlad <64319066+Alena-Vlad@users.noreply.github.com>
2020-05-30 15:50:20 +0300

    Delete lesson_5.5.py

--- lesson_5.5.py
+++
@@ -1,8 +0,0 @@
-with open('54321.txt', 'w') as f:
-    nums = input('Введите целые числа через пробел: ')
-    f.write('Введенные числа: ' + nums + '\n')
-    nums = map(int, nums.split())
-    sum_nums = sum(nums)
-    f.write('Сумма чисел: ' + str(sum_nums))
-    print('Сумма введенных чисел: ', sum_nums)
-print('Все записано в файл')
